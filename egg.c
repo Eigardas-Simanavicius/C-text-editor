bfb21efbce2dd35bdd4ec1824493e0c55bd6fe73
Eigardas Simanavicius <eigardas24@gmail.com>
2025-12-12 22:24:41 +0000

    Can now exit pretty intuitevly imo.

--- egg.c
+++ egg.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-
+#include <unistd.h>
 int main(){
-  printf("hello world \n");
+  char c;
+  char checkexit[2];
+  int quit = 0;
+  printf("Welcome to egg, please press :q to exit \n");
+  while((read(STDIN_FILENO, &c,1) == 1) && quit != 1){
+  if(c == ':'){
+           fgets(checkexit  ,sizeof(checkexit),stdin);
+      if(checkexit[0] == 'q'){    
+        printf("exiting program \n");
+        quit = 1;
+      }
+    }
+  }
   return 0;
-} 
+}
