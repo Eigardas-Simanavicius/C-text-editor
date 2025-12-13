346f402cc839a296ae71854637d2712135d12a9f
Eigardas Simanavicius <eigardas24@gmail.com>
2025-12-13 14:45:30 +0000

    Working on turning raw mode on

--- egg.c
+++ egg.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+void enableRawMode(){
+  // creates a structure to read the current attributes of the terminal
+  struct termios terminalAttributes;
+  // this gets the attributes of the terminal and puts them in the struct
+  tcgetattr(STDIN_FILENO,&terminalAttributes);
+  // turn off ECHO
+  terminalAttributes.c_lflag &= ~(ECHO);
+  // we pass the new attributes back to the terminal 
+  tcsetattr(STDIN_FILENO,TCSAFLUSH,&terminalAttric butes);
+
+}
 int main(){
+  enableRawMode();
   char c;
   char checkexit[2];
   int quit = 0;
