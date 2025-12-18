0b4c34b6167f8c22933fbbebb7c5d12555353d3b
Eigardas Simanavicius <eigardas24@gmail.com>
2025-12-18 19:32:05 +0000

    Some more stuff

--- egg.c
+++ egg.c
@@ -5,13 +5,16 @@
  #include <errno.h>
  #include <ctype.h>
  #include <stdio.h>
+
+ // defines //
+ #define CTRL_KEY(k) ((k) & 0x1f)
  struct termios orgAttributes;
 void errorPrint(const char *s){
    // spits an error back at us when something goes wrong.
    perror(s);
    exit(1);
  }
-
+// terminal slop, stuff we need to change the termnial //
  void disableRawMode(){
    // setting terminal back to standard.
    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&orgAttributes) == -1) errorPrint("tcset");
@@ -33,29 +36,23 @@ void errorPrint(const char *s){
    // we pass the new attributes back to the terminal 
    tcsetattr(STDIN_FILENO,TCSAFLUSH,&terminalAttributes);
  }
-
+// init // 
  int main(){
-  enableRawMode();
-   char c = '\0';
+   enableRawMode();
    printf("Welcome to egg, please press :q to exit \n");
+
+   while(1){
+   char c = '\0';
+   
    // we dont need to do the while loop, due to the vmin and vtime , we it will read automatically/?
    read(STDIN_FILENO, &c,1);
+
    if(iscntrl(c)){
        printf("%d \r\n",c);
-     }else{
-       printf("%d ('%c')\r\n", c,c);
-      }
+   }else{
+       printf("%d ('%c')\r\n", c,c);}
 
-   if(c == ':'){
-       read(STDIN_FILENO,&c,1);
-       if(c == 'q'){    
-         printf("exiting program \n");
-         exit(0);
-       }
-       if(c == 'h'){
-         printf("Helping helping helping \n");
-       }
-     }
-   
+  if(c == CTRL_KEY('q')) break;
+   }
    return 0;
 }
